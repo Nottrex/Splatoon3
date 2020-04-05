9a9d40010c860c57491a01f8e950e48bfce292b6
Laurence <lau.nottrex@gmail.com>
2020-04-05 20:15:00 +0200

    Added basic plugin structure

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-public class Main {
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
-	public static void main(String[] args) {
-		System.out.println("HEY");
+public class Main extends JavaPlugin {
+
+	public void onEnable() {
+		load_Config();
+	}
+
+	public void onDisable() {
+		save_Config();
+	}
+
+	public void load_Config() {
+		FileConfiguration config = getConfig();
+
+	}
+
+	public void save_Config() {
+		saveConfig();
 	}
 }
